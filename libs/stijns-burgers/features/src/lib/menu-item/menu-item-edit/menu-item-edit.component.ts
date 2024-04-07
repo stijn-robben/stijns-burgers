7e3bdee8f5df1588757fa73ef810f58e9de787fa
Stijn Robben <stijn.robben2005@gmail.com>
2024-04-07 13:49:50 +0200

    add menuitem is finished

--- libs/stijns-burgers/features/src/lib/menu-item/menu-item-edit/menu-item-edit.component.ts
+++ libs/stijns-burgers/features/src/lib/menu-item/menu-item-edit/menu-item-edit.component.ts
@@ -3,42 +3,49 @@ import { IMenuItem } from '@herkansing-cswp/shared/api';
 import { MenuItemService } from '../menu-item.service';
 import { Subscription, catchError, of, switchMap, tap } from 'rxjs';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { AuthService } from '@herkansing-cswp/auth';
 
 @Component({
   selector: 'stijns-burgers-menu-item-list',
   templateUrl: './menu-item-edit.component.html',
 })
 export class MenuItemEditComponent implements OnInit, OnDestroy {
-  menuitem: IMenuItem | null = null;
-  routeUrl = this.route.snapshot.url.join('/');
-  subscription: Subscription | undefined = undefined;
-    imageError = false;
-  constructor(private route: ActivatedRoute, private menuitemService: MenuItemService, private router: Router) {}
+  menuitem: IMenuItem = {
+    item_type: '',
+    description: '',
+    name: '',
+    price: 0,
+    ingredients: [],
+    allergens: [],
+    img_url: '',
+    reviews: [],
+    _id: '',
+  };
+  routeUrl = '';
+  imageError = false;
+  subscription: Subscription | undefined;
+
+  constructor(
+    private menuitemService: MenuItemService, 
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
-    this.subscription = this.route.paramMap
-      .pipe(
-        switchMap(params => {
-          const menuitemId = params.get('id');
-          if (menuitemId) {
-            return this.menuitemService.read(menuitemId);
-          } else {
-            return of(undefined); 
-          }
-        }),
-        catchError(error => {
-          console.error('Error fetching menuitem:', error);
-          return of(undefined); 
-        })
-      )
-      .subscribe(menuItemData => {
-        if (menuItemData) {
-          this.menuitem = menuItemData;
-          console.log('Editing menuitem:', this.menuitem);
-        } else {
-          console.log('Menu item not found, new menuitem');
-        }
-      });  }
+    this.routeUrl = this.route.snapshot.url.join('');
+    const itemId = this.route.snapshot.paramMap.get('id');
+    if (itemId) {
+      // We are editing an existing item
+      this.subscription = this.menuitemService.read(itemId).subscribe(
+        data => {
+          this.menuitem = data;
+        },
+        error => console.error('Error:', error)
+      );
+    }
+    // If we are creating a new item, menuitem will remain as the empty object
+  }
 
    initializeNewMenuItem(): void {
     this.menuitem = {
@@ -59,9 +66,12 @@ export class MenuItemEditComponent implements OnInit, OnDestroy {
     this.imageError = true;
   }
   ngOnDestroy(): void {
-    if (this.subscription) this.subscription.unsubscribe();
-  }
-
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }  
+  
+  
   private convertToJson(): string {
     if (this.menuitem) {
       return JSON.stringify(this.menuitem);
@@ -79,41 +89,31 @@ export class MenuItemEditComponent implements OnInit, OnDestroy {
   
   save() {
     console.log('Hier komt de save actie');
-    this.menuitem = {
-      _id: this.menuitem?._id || '', 
-      item_type: this.menuitem?.item_type || '', 
-      description: this.menuitem?.description || '', 
-      name: this.menuitem?.name || '', 
-      price: this.menuitem?.price || 0, 
-      ingredients: this.menuitem?.ingredients || [], 
-      allergens: this.menuitem?.allergens || [], 
-      img_url: this.menuitem?.img_url || '',
-      reviews: this.menuitem?.reviews || [], 
-      ...this.menuitem,
-    };
-  
-    const jsonText = this.convertToJson();
+    const jsonText = JSON.stringify(this.menuitem);
     console.log('JSON Text:', jsonText);
     console.log('Route URL:', this.routeUrl)
     
     if (this.routeUrl.includes('new')) {
       console.log('Creating new menuitem');
+      const { _id, ...menuitemWithoutId } = this.menuitem; // remove _id from the menuitem object
+      const jsonText = JSON.stringify(menuitemWithoutId);
+      console.log('JSON Text:', jsonText);
       this.menuitemService.createMenuItem(JSON.parse(jsonText)).subscribe(createMenuItem => {
-        console.log('Updated menuitem:', createMenuItem);
-        this.router.navigate(['/menu']);
+        this.router.navigate(['/menu/' + createMenuItem._id]);
       });
-
-      
-    } else if (this.routeUrl.includes('edit') && this.menuitem && this.menuitem._id) {
+    }
+     else if (this.routeUrl.includes('edit') && this.menuitem && this.menuitem._id) {
       console.log('Updating menuitem');
       this.menuitemService.updateMenuItem(this.menuitem._id, JSON.parse(jsonText)).subscribe(updatedMenuItem => {
         console.log('Updated menuitem:', updatedMenuItem);
-        this.router.navigate(['/menu']);
+        this.router.navigate(['/menu/' + this.menuitem._id]);
       });
     }
   }
+
   cancel() {
-    this.router.navigate(['/menu']);  }
+    this.router.navigate(['/menu']);
+  }
 }
 
 
