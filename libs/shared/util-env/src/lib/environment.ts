dd2137cccd3c9933b5850e8037250c021728d661
Stijn Robben <stijn.robben2005@gmail.com>
2024-04-09 18:13:20 +0200

    updated environment

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
   production: true,
-  dataApiUrl: 'http://localhost:3000/api',
+  dataApiUrl: 'https://cwfr-stijn-robben.azurewebsites.net',
   mongo:
     'mongodb+srv://stijnrobben:kQpnsI921rmhN7Gr@stijns-burgers.qxgfnqv.mongodb.net/?retryWrites=true&w=majority&appName=stijns-burgers',
 };
