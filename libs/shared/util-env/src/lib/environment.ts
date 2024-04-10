025fdd5f0d6ce7e807de7990727ff5b06f63ca9e
Stijn Robben <stijn.robben2005@gmail.com>
2024-04-10 18:10:28 +0200

    updated environment

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -1,7 +1,7 @@
 import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
-  production: true,
+  production: false,
   dataApiUrl: 'http://localhost:3000/api',
   mongo:
     'mongodb+srv://stijnrobben:kQpnsI921rmhN7Gr@stijns-burgers.qxgfnqv.mongodb.net/?retryWrites=true&w=majority&appName=stijns-burgers',
