60777b3cd17162dcab9baf72596b67017889c19f
Stijn Robben <stijn.robben2005@gmail.com>
2024-04-11 13:49:40 +0200

    environment

--- libs/shared/util-env/src/lib/environment.ts
+++ libs/shared/util-env/src/lib/environment.ts
@@ -2,7 +2,7 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
   production: true,
-  dataApiUrl: 'http://localhost:3000/api',
+  dataApiUrl: 'https://stijns-burgers.azurewebsites.net/api',
   mongo:
     'mongodb+srv://stijnrobben:kQpnsI921rmhN7Gr@stijns-burgers.qxgfnqv.mongodb.net/?retryWrites=true&w=majority&appName=stijns-burgers',
   neo4j: {
